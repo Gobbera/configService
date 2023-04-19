786bb6ca0d4131853613a6e13756c0640feda155
Robert <robertlivino08@gmail.com>
2023-04-19 17:57:17 -0300

    commit function defineItem

--- main.js
+++ main.js
@@ -58,82 +58,21 @@ function generatorFile() {
     let emailTopics = document.getElementById('combobox-email-container');
     let topics = emailTopics.children.length;
     let emailTopic = [];
-    //<debug>
-    debugger;
-    //</debug>
-
     while (emailTopic.length < (topics)) {
         emailTopic.push(`{${'<br>'}
         ${tab}${tab}PLACE: '${emailTopics.children[emailTopic.length || 0].children[1].value}'${'<br>'}
         ${tab}}`);
     }
 
-    for (i = 0; i < layoutRadio.length; i++) {
-        if (layoutRadio[i].checked) {
-            definedLayout = `<span class="true">${layoutRadio[i].value}</span>`;
+    function enableItem(item) {
+        let definedItem
+        for (i = 0; i < item.length; i++) {
+            if (item[i].checked) {
+                definedItem = `<span class="true">${item[i].value}</span>`;
+            }
         }
+        return definedItem;
     }
-    for (i = 0; i < ballonRadio.length; i++) {
-        if (ballonRadio[i].checked) {
-            definedBallon = `<span class="true">${ballonRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < topBarRadio.length; i++) {
-        if (topBarRadio[i].checked) {
-            definedTopBar = `<span class="true">${topBarRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < operatorRadio.length; i++) {
-        if (operatorRadio[i].checked) {
-            definedOperator = `<span class="true">${operatorRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < customerRadio.length; i++) {
-        if (customerRadio[i].checked) {
-            definedCustomer = `<span class="true">${customerRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < logoRadio.length; i++) {
-        if (logoRadio[i].checked) {
-            definedLogo = `<span class="true">${logoRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < currentChatRadio.length; i++) {
-        if (currentChatRadio[i].checked) {
-            definedCurrentChat = `<span class="true">${currentChatRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < skypeRadio.length; i++) {
-        if (skypeRadio[i].checked) {
-            definedSkype = `<span class="true">${skypeRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < messengerRadio.length; i++) {
-        if (messengerRadio[i].checked) {
-            definedMessenger = `<span class="true">${messengerRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < telegramRadio.length; i++) {
-        if (telegramRadio[i].checked) {
-            definedTelegram = `<span class="true">${telegramRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < whatsAppRadio.length; i++) {
-        if (whatsAppRadio[i].checked) {
-            definedWhatsapp = `<span class="true">${whatsAppRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < appleBusinessRadio.length; i++) {
-        if (appleBusinessRadio[i].checked) {
-            definedAppleBusiness = `<span class="true">${appleBusinessRadio[i].value}</span>`;
-        }
-    }
-    for (i = 0; i < emailRadio.length; i++) {
-        if (emailRadio[i].checked) {
-            definedEmail = `<span class="true">${emailRadio[i].value}</span>`;
-        }
-    }
-
     output.innerHTML =
         `window.CONFIG = {${'<br>'}
         ${tab}CORPORATE_NAME: '${corpName.value}',${'<br>'}
@@ -143,23 +82,27 @@ function generatorFile() {
         ${tab}URL_FS: '${url_fs.value}',${'<br>'}
         ${tab}LANGUAGE: '${language.value}',${'<br>'}
         ${tab}IS_HOME: '',${'<br>'}
-        ${tab}LAYOUT: '${definedLayout}',${'<br>'}
-        ${tab}BALLON_MESSAGE: '${definedBallon}',${'<br>'}
+        ${tab}LAYOUT: '${enableItem(layoutRadio)}',${'<br>'}
+        ${tab}BALLON_MESSAGE: '${enableItem(ballonRadio)}',${'<br>'}
         ${tab}CHAT: {${'<br>'}
-        ${tab}${tab}TOP_BAR_ENABLE: ${definedTopBar},${'<br>'}
+        ${tab}${tab}TOP_BAR_ENABLE: ${enableItem(topBarRadio)},${'<br>'}
         ${tab}${tab}OPERATOR: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedOperator},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(operatorRadio)},${'<br>'}
         ${tab}${tab}${tab}IMAGE: '${operatorImg.value}',${'<br>'}
-        ${tab}},${'<br>'}
+        ${tab}
+    },${'<br>'}
         ${tab}${tab}CUSTOMER: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedCustomer},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(customerRadio)},${'<br>'}
         ${tab}${tab}${tab}IMAGE: '${customerImg.value}',${'<br>'}
-        ${tab}${tab}}${'<br>'}
-        ${tab}}${'<br>'}
+        ${tab}${tab}
+    }${'<br>'}
+        ${tab}
+}${'<br>'}
         ${tab}LOGO: {${'<br>'}
-        ${tab}${tab}ENABLE: ${definedCustomer},${'<br>'}
+        ${tab}${tab}ENABLE: ${enableItem(logoRadio)},${'<br>'}
         ${tab}${tab}IMAGE: '${logoImg.value}',${'<br>'}
-        ${tab}},${'<br>'}
+        ${tab}
+},${'<br>'}
         ${tab}BACKGROUND_IMAGE: {${'<br>'}
         ${tab}${tab}NEW_CHAT_SESSION: '${ncsBackground.value}',${'<br>'}
         ${tab}${tab}CHAT: '${chatBackground.value}',${'<br>'}
@@ -168,7 +111,8 @@ function generatorFile() {
         ${tab}${tab}NOT_FOUND: '${notFoundBackground.value}',${'<br>'}
         ${tab}${tab}RECONNECT: '${reconnectBackground.value}',${'<br>'}
         ${tab}${tab}QUEUE: '${queueBackground.value}'${'<br>'}
-        ${tab}},${'<br>'}
+        ${tab}
+},${'<br>'}
         ${tab}ICONS: {${'<br>'}
         ${tab}${tab}CHAT: '',${'<br>'}
         ${tab}${tab}TOOLS_ATTACHMENT: '',${'<br>'}
@@ -198,38 +142,47 @@ function generatorFile() {
         ${tab}${tab}COPY: '',${'<br>'}
         ${tab}${tab}RECONNECT: '',${'<br>'}
         ${tab}${tab}CHATBOT: '',${'<br>'}
-        ${tab}},${'<br>'}
+        ${tab}
+},${'<br>'}
         ${tab}CONTACT_MEDIAS: {${'<br>'}
-        ${tab}${tab}ENABLE: ${definedCurrentChat},${'<br>'}
+        ${tab}${tab}ENABLE: ${enableItem(currentChatRadio)},${'<br>'}
         ${tab}${tab}CURRENT_CHAT: {${'<br>'}
         ${tab}${tab}${tab}IMAGE_LINK: '${currentChat.value}',${'<br>'}
-        ${tab}${tab}},${'<br>'}
+        ${tab}${tab}
+    },${'<br>'}
         ${tab}${tab}SKYPE: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedSkype},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(skypeRadio)},${'<br>'}
         ${tab}${tab}${tab}LINK: '${skype.value}'${'<br>'}
-        ${tab}${tab}},${'<br>'}
+        ${tab}${tab}
+    },${'<br>'}
         ${tab}${tab}MESSENGER: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedMessenger},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(messengerRadio)},${'<br>'}
         ${tab}${tab}${tab}LINK: '${messenger.value}'${'<br>'}
-        ${tab}${tab}},${'<br>'}
+        ${tab}${tab}
+    },${'<br>'}
         ${tab}${tab}TELEGRAM: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedTelegram},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(telegramRadio)},${'<br>'}
         ${tab}${tab}${tab}LINK: '${telegram.value}'${'<br>'}
-        ${tab}${tab}},${'<br>'}
+        ${tab}${tab}
+    },${'<br>'}
         ${tab}${tab}WHATSAPP: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedWhatsapp},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(whatsAppRadio)},${'<br>'}
         ${tab}${tab}${tab}LINK: '${whatsapp.value}'${'<br>'}
-        ${tab}${tab}},${'<br>'}
+        ${tab}${tab}
+    },${'<br>'}
         ${tab}${tab}APPLE_BUSINESS: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedAppleBusiness},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(appleBusinessRadio)},${'<br>'}
         ${tab}${tab}${tab}LINK: '${appleBusiness.value}'${'<br>'}
-        ${tab}${tab}},${'<br>'}
+        ${tab}${tab}
+    },${'<br>'}
         ${tab}${tab}EMAIL: {${'<br>'}
-        ${tab}${tab}${tab}ENABLE: ${definedEmail},${'<br>'}
+        ${tab}${tab}${tab}ENABLE: ${enableItem(emailRadio)},${'<br>'}
         ${tab}${tab}${tab}LINK: '${email.value}'${'<br>'}
-        ${tab}${tab}}${'<br>'}
-        ${tab}},${'<br>'}
-        ${tab}COMBOBOX_EMAIL_SUBJECT:[${emailTopic}],${'<br>'}
+        ${tab}${tab}
+    }${'<br>'}
+        ${tab}
+},${'<br>'}
+        ${tab}COMBOBOX_EMAIL_SUBJECT: [${emailTopic}],${'<br>'}
         }`;
 }
 generatorBtn.addEventListener('click', generatorFile);
