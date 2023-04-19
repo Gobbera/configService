dbee8d38559baa37a32cf1b673bc18ffb3e6fde1
Leonardo <leonardo.gobbo@xgen.com.br>
2023-04-19 14:10:38 -0300

    opa

--- addField.js
+++ addField.js
@@ -1,14 +1,17 @@
-const addFieldBtn = document.getElementById('addFieldBtn');
 const comboboxEmailContainer = document.getElementById('combobox-email-container');
 const newField = `<div>
 <label for="">Assunto</label>
 <input type="text" id="email" name="">
-</div>`
-const newBtn = '<input class="plusBtn" type="button" id="addFieldBtn" value="+">'
+</div>`;
+let id = 1;
+let newBtn = `<input class="plusBtn" type="button" id="addFieldBtn${id}" value="+">`;
+let addFieldBtn = document.getElementById('addFieldBtn');
 
 addFieldBtn.addEventListener('click', addNewField);
+
 function addNewField () {
-    addFieldBtn.hidden = true
-    comboboxEmailContainer.insertAdjacentHTML('afterbegin', newField);
+    id += 1;
+    newBtn = `<input class="plusBtn" type="button" id="addFieldBtn${id}" value="+">`;
     comboboxEmailContainer.insertAdjacentHTML('afterbegin', newBtn);
+    comboboxEmailContainer.insertAdjacentHTML('afterbegin', newField);
 }
