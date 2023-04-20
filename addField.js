1177db71c4df48a9c931534861fc0fa8d301922e
Robert <robertlivino08@gmail.com>
2023-04-20 18:04:00 -0300

    commit new chat Fields

--- addField.js
+++ addField.js
@@ -2,6 +2,7 @@ const comboboxEmailContainer = document.getElementById('combobox-email-container
 const sessionInfoContainer = document.getElementById('session-info-container');
 let id = 0;
 let sessionId = 0;
+let newSessionFieldId = 0;
 let newField = `<div id="fieldContainer${id}">
 <label for="">Assunto</label>
 <input type="text" id="subject" name="">
@@ -37,7 +38,6 @@ function removeField() {
     comboboxEmailContainer.removeChild(fieldContainer);
 }
 function addNewSessionFields() {
-    const me = this;
     sessionId += 1;
     let newSessionFields = `<div id="chatSessionFieldContainer${sessionId}">
     <label for="">Chat field</label>
@@ -56,19 +56,41 @@ function addNewSessionFields() {
             ${tab}<label for="fieldsDisable">Disable</label>
             <input onclick="enableField()" type="radio" name="fields${sessionId}" id="fieldsDisable" value=false>
         </div>
-        <div style="display: none" id="fields${sessionId}">
-        ${tab}${tab}<label for="">Select Chat</label>
-        <input type="text" id="chatSessionField" name=""><br>
-        ${tab}${tab}<label for="">CallType field</label>
-        <input type="text" id="chatSessionField" name=""><br>
-        ${tab}${tab}<label for="">language</label>
-        <input type="text" id="chatSessionField" name=""><br>
+        <div style="display: none" id="newSessionFieldsContainer${sessionId}">
+            <div id="newSessionFields${sessionId}">
+            </div>
+            <button onclick="addNewChatField()" id="addNewChatField${sessionId}" class="plusBtn">+</button> <br>
         </div>
     </div>
     <button onclick="removeSessionFields()" id="removeField${sessionId}" class="plusBtn">-</button>
     </div>`;
     sessionInfoContainer.insertAdjacentHTML('beforeend', newSessionFields);
 }
+function removeNewChatField() {
+    const me = this;
+    const sessionFieldId = me.document.activeElement.parentElement;
+    const newSessionFieldsContainer = me.document.getElementById(`newSessionFieldsContainer${me.document.activeElement.id.substring(15)}`).childNodes[1];
+    newSessionFieldsContainer.removeChild(sessionFieldId);
+
+}
+function addNewChatField() {
+    const me = this;
+    const sessionFieldId = me.document.activeElement.id;
+    newSessionFieldId += 1;
+    let sessionField = `<div id="newSessionFields${sessionId}">
+                            ${tab}${tab}<label for="">Select Field</label>
+                                        <select name="type" id="type">
+                                            <option value="name">name</option>
+                                            <option value="phone">phone</option>
+                                            <option value="CPF">CPF</option>
+                                            <option value="email">email</option>
+                                        </select>
+                            <button onclick="removeNewChatField()" id="addNewChatField${sessionId}" class="plusBtn">-</button> <br>
+                        </div>`;
+    let newSessionFields = document.getElementById(`newSessionFields${sessionFieldId.substring(15)}`);
+    newSessionFields.insertAdjacentHTML('beforeend', sessionField);
+}
+
 function removeSessionFields() {
     const me = this;
     fieldContainer = document.getElementById(`chatSessionFieldContainer${me.document.activeElement.id.substring(11)}`);
@@ -79,7 +101,7 @@ function enableField() {
     const me = this;
     const fieldName = me.document.activeElement.name;
     const fieldRadios = me.document.getElementsByName(fieldName);
-    const fieldId = me.document.getElementById(fieldName);
+    const fieldId = me.document.getElementById(`newSessionFieldsContainer${fieldName.substring(6)}`);
     let definedItem
     for (i = 0; i < fieldRadios.length; i++) {
         if (fieldRadios[i].checked) {
